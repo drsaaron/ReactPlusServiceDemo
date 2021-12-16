46f1ca03d9df41813579b29cc896444641cf4c3f
aar1069 <scottaaron@northwesternmutual.com>
2021-12-16 13:36:54 -0600

    change both application and API servers to https

--- apiserver/index.js
+++ apiserver/index.js
@@ -3,16 +3,26 @@
 const path = require('path');
 const express = require("express");
 const config = require('config');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
 const PORT = config.get('API_PORT');
+const SSL_PORT = config.get('API_HTTPS_PORT');
+
+var privateKey = fs.readFileSync('key.pem');
+var certificate = fs.readFileSync('cert.pem');
+
+var credentials = {key: privateKey, cert: certificate};
 
 const app = express();
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
 
 app.get("/api", (req, res) => {
     console.log("got request");
   res.json({ message: "Hello from API server!" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+httpsServer.listen(SSL_PORT, () => { console.log(`API server listening on port ${SSL_PORT} (https)`)});
--- server/index.js
+++ server/index.js
@@ -4,12 +4,24 @@ const path = require('path');
 const express = require("express");
 const axios = require('axios');
 const config = require('config');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
 const PORT = config.get('SERVER_PORT');
-const API_PORT = config.get('API_PORT');
+const API_PORT = config.get('API_HTTPS_PORT');
 const API_HOST = config.get('API_HOST');
+const SSL_PORT = config.get('SERVER_HTTPS_PORT');
+
+var privateKey = fs.readFileSync('key.pem');
+var certificate = fs.readFileSync('cert.pem');
+
+var credentials = {key: privateKey, cert: certificate};
 
 const app = express();
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -17,7 +29,12 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.get("/api", (req, res) => {
     // call the API server
     //  res.json({ message: "Hello from server!" });
-    axios.get(API_HOST + ":" + API_PORT + "/api")
+    const httpsAgent = new https.Agent({
+	rejectUnauthorized: false, // (NOTE: this will disable client verification)
+	cert: certificate,
+	key: privateKey
+    });
+    axios.get(API_HOST + ":" + API_PORT + "/api", {httpsAgent})
 	.then(response => {
 	    console.log("response = " + Object.keys(response));
 	    return res.json(response.data);
@@ -29,6 +46,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+httpsServer.listen(SSL_PORT, () => { console.log(`Application server listening on port ${SSL_PORT} (https)`)});
